1820d20d0bdcd21ef22f5449db02779d5c7bf6e5
Alex Irpan <xelanapri@gmail.com>
2014-05-08 03:09:33 -0700

    Now in a class

--- solver.py
+++ solver.py
@@ -2,124 +2,105 @@
 import time
 import random
 
-def satisfies(eqn, assign):
-    a, b, c, d, e = eqn
-    return (a * assign[b] + c * assign[d] + e) % p == 0
+# according to PyPy docs, this object makes the class new-style
+# and new-style are faster classes
+class HillClimber(object):
+    def __init__(self, fin, time_limit=120):
+        v, e, p = map(int, fin.readline().split())
+        self.v = v
+        self.e = e
+        self.p = p
+        self.time_limit = time_limit
+         # precompute modular inverses
+        self.inverses = [None] + [pow(i, p-2, p) for i in xrange(1, p)]
+        self.equations = [map(int, fin.readline().split()) for _ in xrange(e)]
+        self.prev_energy = 0
+        self.plateau = 0
+        # index i matches to equation indices that include it
+        self.includes = [ [] for _ in xrange(v) ]
+        for i, eqn in enumerate(self.equations):
+            a,b,c,d,e = eqn
+            self.includes[b].append(i)
+            self.includes[d].append(i)
+        fin.close()
+            
+    def satisfies(self, eqn, assign):
+        a, b, c, d, e = eqn
+        return (a * assign[b] + c * assign[d] + e) % self.p == 0
     
-plateau = [0]
+    def local_max(self, state):
+        # if local_max, return False
+        # else, return an (index, value) pair. index is a random index to improve, value is what to set it to.
+        v = self.v
+        p = self.p
+        indices = range(v)
+        random.shuffle(indices)
+        for index in indices:
+            score = [0] * p # number equations satisfied with assignment to index
+            original_val = state[index]
+            for eqn_ind in self.includes[index]:
+                a, b, c, d, e = self.equations[eqn_ind]
+                if index == b:
+                    satisfies = (-self.inverses[a] * (c * state[d] + e)) % p
+                else:
+                    # index == d
+                    satisfies = (-self.inverses[c] * (a * state[b] + e)) % p
+                score[satisfies] += 1
+            best_score = max(score)
+            best_val, best_score = random.choice(filter(lambda x: x[1] == best_score, enumerate(score)))
+            # compare best score to initial value. if 0, then this is a local max for this variable
+            d_score = best_score - score[original_val]
+            if d_score > 0:
+                self.plateau = 0
+                self.prev_energy += d_score
+                return index, best_val
+            # A magic, tweakable constant
+            # larger = more time wasted travelling around, but more likely to find a better solution
+            # on 2.in, in a 2 minute run there is almost always one time where improvement took > 300 iterations
+            # and usually there is only one
+            # too lazy to actually statistically analyze it
+            elif self.plateau < 350 and best_val != original_val:
+                self.plateau += 1
+                return index, best_val
+        return indices[:v//2]
     
-def local_max(state):
-    # if local_max, return False
-    # else, return an (index, value) pair. index is a random index to improve, value is what to set it to.
-    indices = range(v)
-    random.shuffle(indices)
-    for index in indices:
-        score = [0] * p # number equations satisfied with assignment to index
-        original_val = state[index]
-        for eqn_ind in includes[index]:
-            a, b, c, d, e = equations[eqn_ind]
-            if index == b:
-                satisfies = (-inverses[a] * (c * state[d] + e)) % p
-            else:
-                # index == d
-                satisfies = (-inverses[c] * (a * state[b] + e)) % p
-            score[satisfies] += 1
-        best_score = max(score)
-        best_val, best_score = random.choice(filter(lambda x: x[1] == best_score, enumerate(score)))
-        # compare best score to initial value. if 0, then this is a local max for this variable
-        if best_score - score[original_val] > 0:
-            plateau[0] = 0
-            return index, best_val, best_score - score[original_val]
-        # A magic, tweakable constant
-        elif plateau[0] < 350 and best_val != original_val:
-            plateau[0] += 1
-            return index, best_val, 0
-    return indices[:v//2]
-    
-def move(state, prev_energy):
-    # moves the state, returns the index and old value in case it has to roll back
-    # In a reset, returns -1, -1
-    # (avoids copying entire list this way)
-    val = local_max(state)
-    if type(val) is tuple:
-        # not a local max
-        index, value, diff = val
-        changed_index[0] = index
-        state[index] = value
-        prev_energy[0] += diff
-    else:
-        # is a local max
-        # to get away from this, you have to change pretty radically, so...
-        # randomly change half the indices
-        for index in val:
-            state[index] = random.randint(0, p-1)
-        prev_energy[0] = sum(satisfies(eqn, state) for eqn in equations)
+    def move(self, state):
+        # moves the state, returns the index and old value in case it has to roll back
+        # In a reset, returns -1, -1
+        # (avoids copying entire list this way)
+        val = self.local_max(state)
+        if type(val) is tuple:
+            # not a local max
+            index, value = val
+            state[index] = value
+        else:
+            # is a local max
+            # to get away from this, you have to change pretty radically, so...
+            # randomly change half the indices
+            for index in val:
+                state[index] = random.randint(0, self.p-1)
+            self.plateau = 0
+            self.prev_energy = sum(self.satisfies(eqn, state) for eqn in self.equations)
 
-# not used yet
-def run(p_num):
-    t = time.time()
-    count = 0
-    fin = open("../2.in", "r")
-    fout = open("2-p-%d.out" % p_num, "w")
-    v, e, p = map(int, fin.readline().split())
-    # precompute modular inverses
-    inverses = [None] + [pow(i, p-2, p) for i in xrange(1, p)]
-    equations = [map(int, fin.readline().split()) for _ in xrange(e)]
-    prev_energy = [0]
-    # index i matches to equation indices that include it
-    includes = [ [] for _ in xrange(v) ]
-    for i, eqn in enumerate(equations):
-        a,b,c,d,e = eqn
-        includes[b].append(i)
-        includes[d].append(i)
+    # not used yet
+    def run(self, fout):
+        t = time.time()
+        count = 0
+        # initial state
+        state = [random.randint(0, self.p-1) for _ in xrange(self.v)]
+        best_state = None
+        best_ener = 0
+        while time.time() - t < self.time_limit:
+            count += 1
+            self.move(state)
+            if self.prev_energy > best_ener:
+                best_state = list(state)
+                best_ener = self.prev_energy
+                print >> fout, " ".join(map(str, best_state))
+                print >> fout, "Satisfies %d" % best_ener
+                print >> fout, "%d iterations" % count
+                print >> fout, "%f seconds" % (time.time() - t)
+        fout.close()
         
-    # initial state
-    state = [random.randint(0, p-1) for _ in xrange(v)]
-    best_state = None
-    best_ener = 0
-    prev_equations = [satisfies(eqn, state) for eqn in equations]
-    changed_index = [0]
-    while time.time() - t < 120:
-        count += 1
-        move(state, prev_energy)
-        if prev_energy[0] > best_ener:
-            best_state = list(state)
-            best_ener = prev_energy[0]
-            print >> fout, " ".join(map(str, best_state))
-            print >> fout, "Satisfies %d" % best_ener
-            print >> fout, "%d iterations" % count
-            print >> fout, "%f seconds" % (time.time() - t)
-        
-        
-for file in range(2,3):
-    t = time.time()
-    count = 0
-    fin = open("../2.in", "r")
-    v, e, p = map(int, fin.readline().split())
-    # precompute modular inverses
-    inverses = [None] + [pow(i, p-2, p) for i in xrange(1, p)]
-    equations = [map(int, fin.readline().split()) for _ in xrange(e)]
-    prev_energy = [0]
-    # index i matches to equation indices that include it
-    includes = [ [] for _ in xrange(v) ]
-    for i, eqn in enumerate(equations):
-        a,b,c,d,e = eqn
-        includes[b].append(i)
-        includes[d].append(i)
-        
-    # initial state
-    state = [random.randint(0, p-1) for _ in xrange(v)]
-    best_state = None
-    best_ener = 0
-    prev_equations = [satisfies(eqn, state) for eqn in equations]
-    changed_index = [0]
-    while time.time() - t < 120:
-        count += 1
-        move(state, prev_energy)
-        if prev_energy[0] > best_ener:
-            best_state = list(state)
-            best_ener = prev_energy[0]
-            print " ".join(map(str, best_state))
-            print "Satisfies %d" % best_ener
-            print "%d iterations" % count
-            print "%f seconds" % (time.time() - t)
+h = HillClimber(open("../2.in", "r"))
+h.run(open("2-n.out", "w"))
